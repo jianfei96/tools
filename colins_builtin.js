416a120c43f3327059ec4f8046e49ccc45746250
jianfei96 <ookenhi@gmail.com>
2021-02-02 05:47:22 +0900

    edited builtin

--- colins_builtin.js
+++ colins_builtin.js
@@ -8,8 +8,8 @@ class builtin_encn_Collins {
 
     async displayName() {
         let locale = await api.locale();
-        if (locale.indexOf('CN') != -1) return '柯林斯英汉双解(内置)';
-        if (locale.indexOf('TW') != -1) return '柯林斯英漢雙解(內置)';
+        if (locale.indexOf('CN') != -1) return '柯林斯英汉双解(mod)';
+        if (locale.indexOf('TW') != -1) return '柯林斯英漢雙解(mod)';
         return 'Collins EN->CN Dictionary((builtin))';
     }
 
